382137657707e5362b66df34136940faca43b097
zikirovdavron <davron.zikirov.05@gmail.com>
2025-05-28 16:06:32 +0300

    Add files via upload

---
+++ les2hmw/les2.py
@@ -0,0 +1,33 @@
+# task1
+# from datetime import datetime,date,time,timedelta
+
+# now=datetime.now()
+# print(now)
+
+#task2
+# from datetime import datetime,date,time,timedelta
+# now=date(2025,3,25)
+# future=date(2025,4,10)
+# raz=abs(future-now)
+# print(raz)
+
+#task3
+# from datetime import datetime,date,time,timedelta
+# date1 = date(2025, 3, 10)
+# n= date1.strftime("%A")
+# print(n)
+
+
+#task4
+# # from datetime import datetime,date,time,timedelta
+# # now=datetime.now()
+# now=date(2025,3,25)
+# b=now+timedelta(days=10)
+# print(b)
+
+
+# task5
+# from datetime import datetime,date,time,timedelta
+# date1 = date(2025, 3, 10)
+# res = date1.strftime("%d %B %Y")
+# print(res)
